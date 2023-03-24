c2b8e395ef49f02060dca828eb8774745073cec1
FoxyYTs <foxy200442@gmail.com>
2023-03-24 08:47:30 -0500

    Movido

--- Agenda/Pruebas.java
+++ Agenda/Pruebas.java
@@ -2,10 +2,11 @@ import java.time.LocalDateTime;
 
 public class Pruebas {
     public static void main(String[] args) {
+        System.out.println("Hola mundo");
         LocalDateTime fechaYHora = LocalDateTime.now();
         for (int i = 0; i < 100; i++) {
             System.out.println(fechaYHora);
-            System.out.println("Hola buenas");
+            
         }
     }
 }
