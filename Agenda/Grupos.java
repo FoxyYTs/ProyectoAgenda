b8ad69539d6e03531568bbe810fe709a54278377
FoxyYTs <foxy200442@gmail.com>
2023-03-23 21:37:08 -0500

    SUBETE

--- Agenda/Grupos.java
+++ Agenda/Grupos.java
@@ -1,3 +1,32 @@
+import java.util.ArrayList;
+
 public class Grupos {
-    
+
+    private String nombre;
+    private ArrayList<Contactos> contactos;
+
+    public Grupos(String nombre) {
+        this.nombre = nombre;
+        contactos = new ArrayList<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void agregarContacto(Contactos contacto) {
+        contactos.add(contacto);
+    }
+
+    public void eliminarContacto(Contactos contacto) {
+        contactos.remove(contacto);
+    }
+
+    public ArrayList<Contactos> getContactos() {
+        return contactos;
+    }
 }
