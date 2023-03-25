d0aae23424f9737ca26cbe6215a37b3c23a5ca77
FoxyYTs <foxy200442@gmail.com>
2023-03-25 08:35:44 -0500

    fas

--- Agenda/Pruebas.java
+++ Agenda/Pruebas.java
@@ -3,6 +3,7 @@ import java.time.LocalDateTime;
 public class Pruebas {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("5");
         LocalDateTime fechaYHora = LocalDateTime.now();
         for (int i = 0; i < 100; i++) {
             System.out.println(fechaYHora);
