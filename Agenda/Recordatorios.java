622d134685eb87f357e4beb0ac9a0a7fb8386723
FoxyYTs <foxy200442@gmail.com>
2023-03-26 18:56:04 -0500

    Listas

--- Agenda/Recordatorios.java
+++ Agenda/Recordatorios.java
@@ -3,10 +3,12 @@ import java.time.LocalDateTime;
 public class Recordatorios {
     private String mensaje;
     private LocalDateTime fechaHora;
+    public Recordatorios next;
 
     public Recordatorios(String mensaje, LocalDateTime fechaHora){
         this.mensaje = mensaje;
         this.fechaHora = fechaHora;
+        this.next = null;
     }
 
     public String getMensaje(){
