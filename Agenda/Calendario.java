153ef4c0c5fb7aa81bf8d8abae35adcaf7da8eb5
Jomacogo <cjva88@gmail.com>
2023-03-26 19:34:11 -0500

    carlos

--- Agenda/Calendario.java
+++ Agenda/Calendario.java
@@ -1,4 +1,5 @@
 import java.util.Calendar;
+import java.time.LocalDateTime;
 
 public class Calendario {
 
@@ -7,25 +8,28 @@ public class Calendario {
     public Recordatorios headRecordatorio = recordatorios;
     public Recordatorios pointerRecordatorio = headRecordatorio;
 
-    public Calendario() {
-        eventos = new ArrayList<>();
-        recordatorios = new ArrayList<>();
+    public void insertarEvento(String titulo, LocalDateTime fechaHoraInicio, LocalDateTime fechaHoraFin) {
+        Eventos nuevo = new Eventos(titulo, fechaHoraInicio, fechaHoraFin);
+        if (headEvento == null) {
+            headEvento = nuevo;
+        } else {
+            Eventos pointerEventos = headEvento;
+            while (pointerEventos.next != null) {
+                pointerEventos = pointerEventos.next;
+            }
+            pointerEventos.next = nuevo;
+        }
     }
 
-    public void agregarEvento(Eventos evento) {
-        eventos.add(evento);
-    }
-
-    public void eliminarEvento(Eventos evento) {
-        recordatorios.remove(evento);
-    }
-
-    public void agregarRecordatorio(Recordatorios recordatorio) {
-        recordatorios.add(recordatorio);
-    }
-
-    public void eliminarRecordatorio(Recordatorios recordatorio) {
-        recordatorios.remove(recordatorio);
+    public void mostrar() {
+        Eventos pointerEventos = headEvento;
+        while (pointerEventos != null) {
+            System.out.print("Evento [titulo: " + pointerEventos.getTitulo() + ", Fecha y Hora de inicio: "
+                    + pointerEventos.getFechaHoraInicio() + ", Fecha y hora de Fin: " + pointerEventos.getFechaHoraFin()
+                    + "]");
+            pointerEventos = pointerEventos.next;
+        }
+        System.out.println();
     }
 
     public ArrayList getEvento() {
