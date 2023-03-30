37bb53168148f5edd44b807f1565d2985c439858
Jomacogo <cjva88@gmail.com>
2023-03-29 19:27:15 -0500

    Agenda

--- Agenda/Agenda.java
+++ Agenda/Agenda.java
@@ -1,6 +1,39 @@
-public class Agenda{
-    public static void main(String[]args){
-        System.out.println("Hola Mundo");
-        System.out.println("Pedro");
+public class Agenda {
+
+    public Eventos headContactos;
+    public Eventos pointerContactos = headContactos;
+    public Recordatorios headGrupos;
+    public Recordatorios pointerGrupos = headGrupos;
+    public Calendario calendario;
+    public Configuracion configuracion;
+
+    public void agregarContactos(String nombre, String apellido, String correo, String telefono) {
+        Contactos nuevo = new Contactos(nombre, apellido, correo, telefono);
+        if (headContactos == null) {
+            headContactos = nuevo;
+        } else {
+            Eventos pointerContactos = headContactos;
+            while (pointerContactos.next != null) {
+                pointerContactos = pointerContactos.next;
+            }
+            pointerContactos.next = nuevo;
+        }
+    }
+
+    public void eliminarContacto(String nombre) {
+        if (headContactos == null) {
+            return;
+        }
+        if (headContactos.getTitulo() == nombre) {
+            headContactos = headContactos.next;
+            return;
+        }
+        Eventos pointerContactos = headContactos;
+        while (pointerContactos.next.getTitulo() != nombre && pointerContactos.next != null) {
+            pointerContactos = pointerContactos.next;
+        }
+        if (pointerContactos.next != null) {
+            pointerContactos.next = pointerContactos.next.next;
+        }
     }
 }
