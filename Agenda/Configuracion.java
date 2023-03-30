1c18dce706ca80d3a9e6ca04f0619af6015b1a55
FoxyYTs <foxy200442@gmail.com>
2023-03-29 19:13:13 -0500

    sufrimiento gratis

--- Agenda/Configuracion.java
+++ Agenda/Configuracion.java
@@ -2,9 +2,9 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Configuracion {
-    private String formatoFecha = "dd/MM/yyyy HH:mm";
+    private static String formatoFecha;
     private DateTimeFormatter formatoFechaHora;
-    private String idioma;
+    private static String idioma;
 
     public Configuracion(String formatoFecha, String idioma) {
         this.formatoFecha = formatoFecha;
@@ -14,13 +14,33 @@ public class Configuracion {
     public String getFormatoFecha() {
         return formatoFecha;
     }
-
-    public void setFormatoFecha(String formaFecha) {
-        formatoFechaHora = DateTimeFormatter.ofPattern(formatoFecha);
+    
+    public String getIdioma() {
+        return idioma;
     }
 
-    public String cambiarHora(LocalDateTime fechaHora){
-        String fechaHoraFormateada = fechaHora.format(formatoFechaHora);
+    public void setFormatoFecha(String formatoFecha) {
+        this.formatoFecha = formatoFecha;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public static String fechaHora(LocalDateTime fechaHoraActual){
+        DateTimeFormatter formatoFechaHora = DateTimeFormatter.ofPattern(formatoFecha);
+        String fechaHoraFormateada = fechaHoraActual.format(formatoFechaHora);
         return fechaHoraFormateada;
     }
+
+    public String saludo(){
+        String saludo = null;
+        if (idioma == "es"){
+            saludo = "Hola";
+        }else if(idioma == "en"){
+            saludo = "Hello";
+        }
+        return saludo;
+    }
+
 }
--- Agenda/Idiomas.java
+++
@@ -1,5 +0,0 @@
-import java.util.Locale;
-
-public class Idiomas {
-    Locale 
-}
