b1c6965e69e539ec09cd653e9ef2d82fe69fa0c7
FoxyYTs <foxy200442@gmail.com>
2023-04-03 09:55:20 -0500

    sube

--- Agenda/Prueba/FechaFormato.java
+++ Agenda/Prueba/FechaFormato.java
@@ -1,26 +1,3 @@
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Scanner;  
-
 public class FechaFormato {
-    
-    /*
-    public static void main(String[] args) {
-        String hola = null;
-        System.out.println(hola);
-        hola = "hola";
-        System.out.println(hola);
-        hola = null;
-        System.out.println(hola);
-    } */
-
-
-
-
-    public static String hora(String formato, LocalDateTime fechaHoraActual){
-        DateTimeFormatter formatoFechaHora = DateTimeFormatter.ofPattern(formato);
-        String fechaHoraFormateada = fechaHoraActual.format(formatoFechaHora);
-        return fechaHoraFormateada;
-    }
     
 }
