d3c9b88a5d95bd8d4e16543a2ab1d44cdf7a31e0
FoxyYTs <foxy200442@gmail.com>
2023-04-23 10:26:00 -0500

    a

--- Agenda/prueba/Main.java
+++ Agenda/prueba/Main.java
@@ -5,14 +5,14 @@ import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
-        String a = "d/M/y h:mm a";
+        String a = "d/M/y h:mm";
         LocalDateTime fechaHoraActual = LocalDateTime.now();
         DateTimeFormatter formato = DateTimeFormatter.ofPattern(a);
         String fechaHoraActualFormateada = fechaHoraActual.format(formato);
         System.out.println("Fecha y hora actual: " + fechaHoraActualFormateada);
 
         DateTimeFormatter formatoFechaHora = DateTimeFormatter.ofPattern(a);
-        LocalDateTime fechaHoraFormateada = LocalDateTime.parse("23/4/2023 10:06 a. m.", formatoFechaHora);
+        LocalDateTime fechaHoraFormateada = LocalDateTime.parse("23/4/2023 10:30 ", formatoFechaHora);
     }
 }   
 
